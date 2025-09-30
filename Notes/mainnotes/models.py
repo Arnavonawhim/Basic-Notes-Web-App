033318434fd55ff40345a9a801a9a64c0b3c09fd
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-09-30 18:50:53 +0530

    added images CRUD

--- Notes/mainnotes/models.py
+++ Notes/mainnotes/models.py
@@ -5,6 +5,7 @@ class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- Notes/mainnotes/views.py
+++ Notes/mainnotes/views.py
@@ -15,14 +15,19 @@ def notes(request):
 def note_add(request):
     title = request.POST.get('title')
     content = request.POST.get('content')
-        
+    image = request.FILES.get('image')
+
     if not title or not content:
         messages.error(request, "Notes Can't be empty")
         return render(request, 'note_add.html')
         
-    Note.objects.create(user=request.user,title=title,content=content)
-    messages.success(request, "Note Was Added")
-    return redirect('notes')
+    Note.objects.create(user=request.user,title=title,content=content,image=image)
+    if not image:
+        messages.success(request, "Note Was Added")
+        return redirect('notes')
+    else:
+        messages.success(request,'Note Was Added Along With Image')
+        return redirect('notes')
 
 @login_required(login_url='login_page')
 def note_delete(request, note_id):
@@ -37,13 +42,15 @@ def note_edit(request,note_id):
     current_note= get_object_or_404(Note,id=note_id,user=request.user)
     new_title=request.POST.get('title')
     new_content=request.POST.get('content')
+    new_image=request.FILES.get('image')
 
-    if not new_title or not new_content:
+    if not new_title or not new_content or not new_image:
         messages.error(request,"Update Something")
         return render(request, 'note_edit.html', {'note': current_note})
     
     current_note.title=new_title
     current_note.content=new_content
+    current_note.image=new_image
     current_note.save()
     messages.success(request,"Note was updated")
     return redirect('notes')
