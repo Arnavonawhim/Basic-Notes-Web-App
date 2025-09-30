7bd8df98b38840820a0108a72a9fde23c5b517b1
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-09-30 17:31:43 +0530

    Added edit_update

--- Notes/mainnotes/urls.py
+++ Notes/mainnotes/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("notes/",views.notes,name="notes"),
     path("notes/add/", views.note_add, name="note_add"),
-    path("notes/delete/<int:note_id>/", views.note_delete, name="note_delete")
+    path("notes/delete/<int:note_id>/", views.note_delete, name="note_delete"),
+    path("notes/edit/<int:note_id>/", views.note_edit, name="note_edit"),
 ]
--- Notes/mainnotes/views.py
+++ Notes/mainnotes/views.py
@@ -26,8 +26,24 @@ def note_add(request):
 
 @login_required(login_url='login_page')
 def note_delete(request, note_id):
-    current_note = get_object_or_404(Note, id=note_id, user=request.user)
+    current_note = get_object_or_404(Note,id=note_id,user=request.user)
     if request.method == 'POST':
         current_note.delete()
         messages.success(request, "Note Deleted")
         return redirect('notes')
+    
+@login_required(login_url='login_page')
+def note_edit(request,note_id):
+    current_note= get_object_or_404(Note,id=note_id,user=request.user)
+    new_title=request.POST.get('title')
+    new_content=request.POST.get('content')
+
+    if not new_title or not new_content:
+        messages.error(request,"Update Something")
+        return render(request, 'note_edit.html', {'note': current_note})
+    
+    current_note.title=new_title
+    current_note.content=new_content
+    current_note.save()
+    messages.success(request,"Note was updated")
+    return redirect('notes')
