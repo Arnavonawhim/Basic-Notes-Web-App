ecd2f33df77199efb6f0960c27f4756e5f3f30de
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-09-30 11:21:38 +0530

    Working User Registration Login and Authentication

--- Notes/userauth/urls.py
+++ Notes/userauth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("",views.home,name="home"),
+    path("",views.login_page,name="login_page"),
     path("register/",views.register,name="register")
 ]
--- Notes/userauth/views.py
+++ Notes/userauth/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 
-def home(request):
+def login_page(request):
+    email=request.POST.get("email")
+    password=request.POST.get("password")
+    
+    if not User.objects.filter(email=email).exists():
+        messages.error(request,"Email Is not registered")
+        return render(request,'login.html')
+    else:
+        person=User.objects.get(email=email)
+
+    person = authenticate(request, username=person.username, password=password)
+    if person is not None:
+        login(request,person)
+        return redirect('notes')
+    else:
+        messages.error(request, "Invalid email or password") 
+        return render(request, 'login.html')
+    
     return render(request,'login.html')
+
 
 def register(request):
     name=request.POST.get('name')
@@ -14,12 +33,14 @@ def register(request):
         messages.error(request,"Enter All The Details")
         return render(request,'Register.html')
     
-    if User.Objects.filter(username=email).exists():
+    if User.objects.filter(username=email).exists():
         messages.error(request,"User Already Registered Go To Login Page")
-        return render(request,'register.html')
+        return render(request,'Register.html')
     
-    person=User.objects.create_user(username=name,email=email,password=password,first_name=name)
-    #login()
-
+    person=User.objects.create_user(username=email,email=email,password=password,first_name=name)
+    return render(request,'Register.html')
     #return redirect notes
 
+def logout_person(request):
+    logout(request)
+    return redirect('login_page')
